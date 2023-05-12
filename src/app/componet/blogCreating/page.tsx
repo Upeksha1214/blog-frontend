f3e3236394c9cad8c08550d779023dcbec2b1f63
Upeksha1214 <upekshasachintha@gmail.com>
2023-05-12 07:59:52 -0700

    up date button bg is updated

--- src/app/componet/blogCreating/page.tsx
+++ src/app/componet/blogCreating/page.tsx
@@ -90,7 +90,7 @@ export default function page() {
                         </label>
                     </div>
 
-                    <button className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                    <button className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-emerald-500 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-emerald-900">
                         Update Post
                     </button>
                 </div>
